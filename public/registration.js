969701f8565bbb2e6ee959840d60f8cc3a824c4b
ISTERQ <131639934+ISTERQ@users.noreply.github.com>
2025-04-09 23:06:47 +0900

    Update registration.js

--- public/registration.js
+++ public/registration.js
@@ -1,36 +1,30 @@
 const registerForm = document.getElementById('registrationForm');
-registerForm.addEventListener('submit', registerUser);
 
-function registerUser(event) {
-    event.preventDefault(); // Остановите стандартное поведение формы
+registerForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
 
     const username = document.getElementById('registerUsername').value;
     const email = document.getElementById('registerEmail').value;
     const password = document.getElementById('registerPassword').value;
 
-    // Формируем данные для отправки
     const data = { username, email, password };
 
-    // Отправляем данные на сервер
-    fetch('https://fastfoodmania-github-io.onrender.com/api/register', { // Используйте правильный URL вашего сервера
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.success) {
-            alert('Регистрация успешна!'); // Успешная регистрация
-        } else {
-            alert('Ошибка: ' + data.message); // Ошибка регистрации
-        }
-    })
-    .catch(error => {
-        console.error('Ошибка:', error);
-        alert('Ошибка при регистрации. Пожалуйста, попробуйте еще раз.');
-    });
-}
+    try {
+        const response = await fetch('https://fastfoodmania-api.onrender.com/register', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data)
+        });
 
-document.getElementById('registrationForm').addEventListener('submit', registerUser);
+        const result = await response.json();
+        if (response.ok) {
+            alert("Регистрация успешна!");
+            document.getElementById('loginModal').style.display = 'none';
+        } else {
+            alert("Ошибка: " + result.message);
+        }
+    } catch (err) {
+        console.error('Ошибка:', err);
+        alert('Ошибка при регистрации.');
+    }
+});
