286a0df6c01775392ca8c5521157476ffe0db014
ISTERQ <131639934+ISTERQ@users.noreply.github.com>
2025-05-10 21:37:46 +0900

    Update server.js

--- server.js
+++ server.js
@@ -374,3 +374,14 @@ app.get('/orders/:userId', async (req, res) => {
     res.status(500).json({ message: "Ошибка сервера" });
   }
 });
+
+
+app.get('/api/orders/:userId', async (req, res) => {
+  try {
+    const orders = await Order.find({ userId: req.params.userId }).sort({ createdAt: -1 });
+    res.json(orders);
+  } catch (err) {
+    console.error("Ошибка получения заказов:", err);
+    res.status(500).json({ message: "Ошибка сервера" });
+  }
+});
