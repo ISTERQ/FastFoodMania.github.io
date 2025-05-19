8e87dd5d249894a6b9c166cc08d870513eb30d37
ISTERQ <131639934+ISTERQ@users.noreply.github.com>
2025-05-19 22:11:32 +0900

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -2,13 +2,7 @@ const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  items: [
-    {
-      name: String,
-      quantity: Number,
-      price: Number,
-    }
-  ],
+  items: [{ name: String, quantity: Number, price: Number }],
   total: Number,
   createdAt: { type: Date, default: Date.now }
 });
