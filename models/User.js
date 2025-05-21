740e1dd10653f8cefd914adaa3b3e13788a15a0c
ISTERQ <131639934+ISTERQ@users.noreply.github.com>
2025-05-21 20:16:12 +0900

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  orders: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order" }]
+  email: { type: String, required: true },
+  name: { type: String, default: "" },
+  city: { type: String, default: "" },
+  orders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }]  // Связь с заказами
 });
 
-module.exports = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
+module.exports = User;
