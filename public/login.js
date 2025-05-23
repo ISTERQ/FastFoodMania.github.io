621ee160f474a6ef317656c7908b0cbf9d825e26
ISTERQ <131639934+ISTERQ@users.noreply.github.com>
2025-05-23 21:51:45 +0900

    Update login.js

--- public/login.js
+++ public/login.js
@@ -1,18 +1,18 @@
+
 document.getElementById("loginForm").addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const email = document.getElementById("loginEmail").value.trim();
   const password = document.getElementById("loginPassword").value;
 
-  // Сохраняем введённые данные
+  // Сохраняем данные в localStorage
   localStorage.setItem("accessToken", "fakeToken");
   localStorage.setItem("userId", "fakeUser");
   localStorage.setItem("username", email);
-  localStorage.setItem("password", password); // сохраняем для проверки в профиле
+  localStorage.setItem("password", password);
 
   alert("Вход выполнен!");
 
-  // Закрываем окно входа
   document.getElementById("loginModal").style.display = "none";
   document.getElementById("modalOverlay").style.display = "none";
 });
