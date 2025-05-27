651909b6f69b78cdb70d6d52732a7f20e85956f5
ISTERQ <131639934+ISTERQ@users.noreply.github.com>
2025-05-27 20:13:02 +0900

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,16 +1,17 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  items: [
-    {
-      name: String,
-      quantity: Number,
-      price: Number,
-    }
-  ],
-  total: Number,
+  userId: { type: String, required: true },
+  items: [{
+    id: String,
+    name: String,
+    price: Number,
+    quantity: Number
+  }],
+  total: { type: Number, required: true },
+  phone: String,
+  address: String,
   createdAt: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("Order", orderSchema);
+module.exports = mongoose.model('Order', orderSchema);
