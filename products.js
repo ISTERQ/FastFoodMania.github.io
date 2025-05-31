801708108e7a745afaffcba7499b45aa683ef39f
ISTERQ <131639934+ISTERQ@users.noreply.github.com>
2025-05-31 17:22:15 +0900

    Create products.js

---
+++ products.js
@@ -0,0 +1,351 @@
+module.exports = [
+  // Бургеры
+  {
+    id: "burger1",
+    category: "burgers",
+    name: "Mexican Burger",
+    description: "Этот бургер в мексиканском стиле наполнен ароматом порошка чили, кинзы и перца халапеньо. Подается на булочках с листьями салата.",
+    price: 350,
+    image: "img/menu-modern-1-310x260.png"
+  },
+  {
+    id: "burger2",
+    category: "burgers",
+    name: "Double Burger",
+    description: "Две фирменные говяжьи котлеты, обжаренные в огне, увенчанные простым слоем расплавленного американского сыра и нарезанными солеными огурцами.",
+    price: 350,
+    image: "img/menu-modern-5-310x260.webp"
+  },
+  {
+    id: "burger3",
+    category: "burgers",
+    name: "California Burger",
+    description: "Ростки люцерны венчают эти сочные бургеры. Сочетание обычного индюшиного фарша и индюшиной грудки обеспечивает превосходную текстуру.",
+    price: 350,
+    image: "img/menu-modern-6-310x260.webp"
+  },
+  {
+    id: "burger4",
+    category: "burgers",
+    name: "Mini Burger",
+    description: "Мини-бургеры, известные многим как слайдеры,- это самый горячий тренд в кулинарии, который охватил всю страну: их размер в два раза меньше классического бургера.",
+    price: 350,
+    image: "img/menu-modern-3-310x260.png"
+  },
+  {
+    id: "burger5",
+    category: "burgers",
+    name: "Classic Burger",
+    description: "Куриная говядина, приготовленная на углях с луком на гриле, острый перец, листья салата, помидоры, двойной американский сырный соус чеддер.",
+    price: 350,
+    image: "img/menu-modern-2-310x260.webp"
+  },
+  {
+    id: "burger6",
+    category: "burgers",
+    name: "Chicken Burger",
+    description: "Наш «Чикен Бургер» состоит из пикантной куриной котлеты-гриль, увенчанной сочными помидорами, свежим салатом и майонезом.",
+    price: 350,
+    image: "img/menu-modern-4-310x260.png"
+  },
+
+  // Пиццы
+  {
+    id: "pizza1",
+    category: "pizza",
+    name: "Hawaiian",
+    description: "Свежий ананас, копченая ветчина из яблочного дерева и нарезанный лук-шалот делают эту пиццу №1 среди наших самых маленьких посетителей.",
+    price: 350,
+    image: "img/pizzas-1-310x260.png"
+  },
+  {
+    id: "pizza2",
+    category: "pizza",
+    name: "Sicilian",
+    description: "В этой ароматной пицце с острой маринарой, итальянской колбасой, острой ветчиной Капикола, салями и Моцареллой деревенский стиль сочетается с изысканным.",
+    price: 350,
+    image: "img/pizzas-2-310x260.png"
+  },
+  {
+    id: "pizza3",
+    category: "pizza",
+    name: "Classic Cheese",
+    description: "Большая круглая пицца от QuickFood с начинкой из 100% сыров Моцарелла и Мюнстер, горячая из печи и готовая к вашему приходу.",
+    price: 350,
+    image: "img/pizzas-3-310x260.png"
+  },
+  {
+    id: "pizza4",
+    category: "pizza",
+    name: "Margherita",
+    description: "Классика, которую любят все. Попробуйте нашу «Маргериту» с итальянскими помидорами, свежей Моцареллой, свежим базиликом и Пармезаном.",
+    price: 350,
+    image: "img/pizzas-4-310x260.png"
+  },
+  {
+    id: "pizza5",
+    category: "pizza",
+    name: "Neapolitan",
+    description: "Одна из самых популярных наших блюд, эта пицца включает в себя свежие помидоры, сыр, масло и чеснок и подается с многочисленными начинками.",
+    price: 350,
+    image: "img/pizzas-5-310x260.png"
+  },
+  {
+    id: "pizza6",
+    category: "pizza",
+    name: "Pepperoni",
+    description: "Все мирный фаворит всех времен! Ощутите вкус оригинальной Пепперони.",
+    price: 350,
+    image: "img/pizzas-6-310x260.png"
+  },
+
+  // Тосты
+  {
+    id: "toast1",
+    category: "toast",
+    name: "Tomato Toast",
+    description: "Наслаждайтесь этим удивительным сочетанием сыра и помидоров с небольшим количеством салата и огурцов на свежеиспеченном хлебе.",
+    price: 350,
+    image: "img/toast-1-310x260.png"
+  },
+  {
+    id: "toast2",
+    category: "toast",
+    name: "Cheese Toast",
+    description: "Этот тост - один из самых популярных в нашем меню, и неудивительно, почему - он такой же вкусный, оригинальный и простой в приготовлении.",
+    price: 350,
+    image: "img/toast-2-310x260.png"
+  },
+  {
+    id: "toast3",
+    category: "toast",
+    name: "Beef Toast",
+    description: "Нежный, ароматный и идеально приправленный ростбиф, тонко нарезанный, с копченым сыром Гауда и подается на багете.",
+    price: 350,
+    image: "img/toast-3-310x260.png"
+  },
+  {
+    id: "toast4",
+    category: "toast",
+    name: "Italian Toast",
+    description: "Ощутите вкус настоящей итальянской кухни с этим тостом, в состав которого входят сыр Проволоне, свежие помидоры и свежеиспеченный хлеб.",
+    price: 350,
+    image: "img/toast-4-310x260.png"
+  },
+  {
+    id: "toast5",
+    category: "toast",
+    name: "Mediterranean Toast",
+    description: "Теплый, поджаристый и сытный, этот тост удовлетворит ваш аппетит благодаря Zippy Hummus, жареному красному перцу и поджаренному бекону.",
+    price: 350,
+    image: "img/toast-5-310x260.png"
+  },
+  {
+    id: "toast6",
+    category: "toast",
+    name: "Chicken Toast",
+    description: "Мы переосмыслили оригинальный сэндвич с курицей, и теперь вы можете насладиться вкусом наших куриных тостов с помидорами черри и салатом.",
+    price: 350,
+    image: "img/toast-6-310x260.png"
+  },
+
+  // Салаты
+  {
+    id: "salads1",
+    category: "salads",
+    name: "Buffalo Bleu",
+    description: "Нарезанный ромэн и айсберг, натуральная курица, оригинальный острый соус «Буффало Нью-Йорк», виноградные помидоры и банановый перец.",
+    price: 350,
+    image: "img/salads-1-310x260.png"
+  },
+  {
+    id: "salads2",
+    category: "salads",
+    name: "Greek Salad",
+    description: "Огурцы, виноградные помидоры, красный лук, банановый перец, черные оливки и сыр фета с бальзамической заправкой винегрет.",
+    price: 350,
+    image: "img/salads-2-310x260.png"
+  },
+  {
+    id: "salads3",
+    category: "salads",
+    name: "Turkey Salad",
+    description: "Нарезанный ромэн и айсберг, паста радиатор, обжаренная индейка, хрустящий бекон, помидоры и пахтовая заправка ранч",
+    price: 350,
+    image: "img/salads-3-310x260.png"
+  },
+  {
+    id: "salads4",
+    category: "salads",
+    name: "Mediterranean",
+    description: "Весенний микс, натуральная курица, киноа, маслины, маринованные помидоры, семена подсолнечника, сыр фета и бальзамический винегрет.",
+    price: 350,
+    image: "img/salads-4-310x260.png"
+  },
+  {
+    id: "salads5",
+    category: "salads",
+    name: "Farmhouse salad",
+    description: "Капуста, весенний микс, запеченная индейка, запеченный баттернат сквош, запеченная брюссельская капуста, глазированные орехи и козий сыр.",
+    price: 350,
+    image: "img/salads-5-310x260.png"
+  },
+  {
+    id: "salads6",
+    category: "salads",
+    name: "Chicken Caprese",
+    description: "ОСердечки ромэн, запеченная курица, авокадо, нарезанные помидоры, свежая моцарелла и бальзамический винегрет прекрасно сочетаются.",
+    price: 350,
+    image: "img/salads-6-310x260.png"
+  },
+
+  // Десерты
+  {
+    id: "desserts1",
+    category: "desserts",
+    name: "Pancakes",
+    description: "Получите все вкусности этого американского блюда за половину времени. Насладитесь незабываемым вкусом наших блинчиков!",
+    price: 350,
+    image: "img/deserts-1-310x260.png"
+  },
+  {
+    id: "desserts2",
+    category: "desserts",
+    name: "Chocolate Fudge Cake",
+    description: "Этот торт, украшенный кусочками шоколадного брауни и подаваемый с ванильным мороженым, просто необходим, если вы любите вкусные десерты.",
+    price: 350,
+    image: "img/deserts-2-310x260.png"
+  },
+  {
+    id: "desserts3",
+    category: "desserts",
+    name: "French macaroons",
+    description: "Французские макаруны - это тонкое ароматное печенье безе, в которое вложена начинка.",
+    price: 350,
+    image: "img/deserts-3-310x260.png"
+  },
+  {
+    id: "desserts4",
+    category: "desserts",
+    name: "Blueberry muffins",
+    description: "Большие черничные маффины с сахарной корочкой. Черника и сладкое тесто потрясающе вкусны.",
+    price: 350,
+    image: "img/deserts-4-310x260.png"
+  },
+  {
+    id: "desserts5",
+    category: "desserts",
+    name: "Donuts",
+    description: "Домашние пончики с начинкой из восхитительного ванильного крем-пудинга и сладкой белой глазурью приведут вас в восторг.",
+    price: 350,
+    image: "img/deserts-5-310x260.png"
+  },
+  {
+    id: "desserts6",
+    category: "desserts",
+    name: "Ice Cream",
+    description: "Наше классическое ванильное мороженое премиум-класса со сладким кремом - это чистое удовольствие.",
+    price: 350,
+    image: "img/deserts-6-310x260.png"
+  },
+
+  // Комбо
+  {
+    id: "combo1",
+    category: "combo",
+    name: "Сытное трио",
+    description: "Погрузитесь в мир гастрономического наслаждения с нашим идеальным комбо, которое включает сочный бургер из свежайших ингредиентов, легкий освежающий салат с хрустящими овощами и классическую Кока-Колу.",
+    price: 350,
+    image: "img/combo-1-310x260.png"
+  },
+  {
+    id: "combo2",
+    category: "combo",
+    name: "Тайна Ночи",
+    description: "Коктейль Blue Moon с его яркими нотами голубого ликера, насыщенный и яркий Red Dawn с фруктовыми оттенками ягод, и изысканные французские макароны с разнообразными кремами.",
+    price: 350,
+    image: "img/combo-2-310x260.png"
+  },
+  {
+    id: "combo3",
+    category: "combo",
+    name: "Гурманское наслаждение",
+    description: "«Гурманское наслаждение»,включает сочный бургер с нежной котлетой, плавленым сыром и хрустящими огурцами, порцию сладкого мороженого и освежающий спрайт для истинного удовольствия!",
+    price: 350,
+    image: "img/combo-3-310x260.png"
+  },
+  {
+    id: "combo4",
+    category: "combo",
+    name: "Гурманская Тройка",
+    description: "«Гурманская Тройка» в наборе вы найдете сочный бургер с двойной котлетой и свежими овощами, идеально дополняемый освежающей Кока-Колой. Завершите трапезу легким и воздушным маффином на десерт, который подарит сладкое наслаждение!",
+    price: 350,
+    image: "img/combo-4-310x260.png"
+  },
+  {
+    id: "combo5",
+    category: "combo",
+    name: "Ягодный взрыв с куриным акцентом",
+    description: "Попробуйте наше уникальное комбо, включающее хрустящий куриный тост с пикантным соусом, освежающий лимонад и легкий салат из свежих ягод для идеального обеда или перекуса!",
+    price: 350,
+    image: "img/combo-5-310x260.png"
+  },
+  {
+    id: "combo6",
+    category: "combo",
+    name: "Триумф Пиццы",
+    description: "Комбо \"Триумф Пиццы\" предлагает вам три великолепные пиццы: классическую Маргариту с ароматным базиликом и томатным соусом, пикантную Пепперони с восхитительными слоями колбасы и сыром, а также свежую Вегетарианскую, щедро усыпанную сезонными овощами.",
+    price: 350,
+    image: "img/combo-6-310x260.png"
+  },
+
+  // Напитки
+  {
+    id: "drinks1",
+    category: "drinks",
+    name: "Diet Coke",
+    description: "Попробуйте морозную и освежающую диетическую колу без калорий, освежающую классику, которая дополнит любое блюдо из нашего меню.",
+    price: 350,
+    image: "img/drinks-1-310x260.png"
+  },
+  {
+    id: "drinks2",
+    category: "drinks",
+    name: "Blue Moon",
+    description: "Коктейль Blue Moon - это веселый замороженный мартини, в состав которого входят голубой кюрасао, ванильный сироп, свежий апельсиновый сок и взбитые сливки.",
+    price: 350,
+    image: "img/drinks-2-310x260.webp"
+  },
+  {
+    id: "drinks3",
+    category: "drinks",
+    name: "Sparkling Water",
+    description: "Отличный выбор для тех, кто хочет освежиться в жаркую погоду. Широкий выбор вкусов!",
+    price: 350,
+    image: "img/drinks-3-310x260.webp"
+  },
+  {
+    id: "drinks4",
+    category: "drinks",
+    name: "Red Dawn",
+    description: "The Red Dawn - крепкий и ароматный коктейль, состоящий из Southern Comfort, лимонно-лаймовой содовой и клюквенного сока.",
+    price: 350,
+    image: "img/drinks-4-310x260.png"
+  },
+  {
+    id: "drinks5",
+    category: "drinks",
+    name: "Pina Colada",
+    description: "Наша фирменная замороженная Пина Колада с ромом Don Q, ананасовым соком и Coco Lopez, увенчанная поджаренным кокосом.",
+    price: 350,
+    image: "img/drinks-5-310x260.webp"
+  },
+  {
+    id: "drinks6",
+    category: "drinks",
+    name: "Classic Lemonade",
+    description: "Освежающий лимонад с нотками мяты и имбиря. Освежающие лимонные напитки - идеальный глоток, когда вы охлаждаетесь.",
+    price: 350,
+    image: "img/drinks-6-310x260.png"
+  }
+];
